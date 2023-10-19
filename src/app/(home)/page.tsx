5bea24a7cd8901383d460f80c4589490fa6a1080
valdenicio22 <valdenicio22@gmail.com>
2023-10-19 19:55:37 +0200

    feat: add PromoBanner, SectionTitle components

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
-import { Banner } from "@/components/ui/banner";
+import { SectionTitle } from "@/components/ui/SectionTitle";
 import { prismaClient } from "@/lib/prisma";
 import Categories from "./components/categories";
+import { PromoBanner } from "./components/promo-banner";
 import { SliderProductList } from "./components/slider-product-list";
 
 export default async function Home() {
@@ -11,16 +12,36 @@ export default async function Home() {
       },
     },
   });
+  const keyboards = await prismaClient.product.findMany({
+    where: {
+      category: {
+        slug: "keyboards",
+      },
+    },
+  });
   return (
     <div className="max-w-[390px]">
-      <div className="my-8 px-5">
-        <Banner src="/banner-home-01.png" alt="discount banner" />
-      </div>
-      <div className="px-5">
-        <Categories />
-      </div>
-      <div className="mt-8">
-        <SliderProductList products={offers} />
+      <div className="flex flex-col gap-8">
+        <div className="px-5">
+          <PromoBanner src="/banner-home-01.png" alt="discount banner" />
+        </div>
+        <div className="px-5">
+          <Categories />
+        </div>
+
+        <div>
+          <SectionTitle>Ofertas</SectionTitle>
+          <SliderProductList products={offers} />
+        </div>
+
+        <div className="px-5">
+          <PromoBanner src="/banner-home-02.png" alt="discount banner" />
+        </div>
+
+        <div>
+          <SectionTitle>Teclados</SectionTitle>
+          <SliderProductList products={keyboards} />
+        </div>
       </div>
     </div>
   );
