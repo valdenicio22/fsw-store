9735a60f6d1e081663fd2a10e391b398aff8895a
valdenicio22 <valdenicio22@gmail.com>
2023-10-19 20:05:36 +0200

    feat: add Footer component to layout

---
+++ src/components/ui/footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <div className="bg-accent px-8 py-4 text-xs opacity-75">
+      © 2023 Copyright <span className="font-semibold">FSW Store</span>
+    </div>
+  );
+};
