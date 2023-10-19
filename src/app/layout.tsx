c614e52c4ad64d1f8ea06dddc1f1e86fd9e24368
valdenicio22 <valdenicio22@gmail.com>
2023-10-20 00:45:30 +0200

    feat: ensure product price are wrapped in one line

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import {Footer} from "@/components/ui/footer";
+import { Footer } from "@/components/ui/footer";
 import { Header } from "@/components/ui/header";
 import { AuthProvider } from "@/providers/auth";
 import type { Metadata } from "next";
@@ -20,11 +20,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthProvider>
-          <Header />
-          {children}
-          <Footer />
-        </AuthProvider>
+        <div className="flex h-full flex-col">
+          <AuthProvider>
+            <Header />
+            <div className="flex-1">{children}</div>
+            <Footer />
+          </AuthProvider>
+        </div>
       </body>
     </html>
   );
