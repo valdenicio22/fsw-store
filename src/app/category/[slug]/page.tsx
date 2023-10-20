fe77af6ebbad61c2631eb9ca7f1541111297a17f
valdenicio22 <valdenicio22@gmail.com>
2023-10-20 08:48:31 +0200

    feat: add category page

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,5 +1,49 @@
-const CategoryPage = ({ params }: any) => {
-  return <div>{params.slug}</div>;
+import { Badge } from "@/components/ui/badge";
+import { ProductItem } from "@/components/ui/product-item";
+import { CATEGORY_ICON } from "@/constants/category-icon";
+import { computeProductTotalPrice } from "@/helpers/product";
+import { prismaClient } from "@/lib/prisma";
+
+type CategoryProductsProps = {
+  params: {
+    slug: string;
+  };
 };
 
-export default CategoryPage;
+const CategoryProducts = async ({ params }: CategoryProductsProps) => {
+  const category = await prismaClient.category.findFirst({
+    where: {
+      slug: params.slug,
+    },
+    include: {
+      products: true,
+    },
+  });
+
+  if (!category) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col gap-8 p-5">
+      <Badge
+        className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
+        variant="outline"
+      >
+        {CATEGORY_ICON[params.slug as keyof typeof CATEGORY_ICON]}
+        {category.name}
+      </Badge>
+
+      <div className="grid grid-cols-2 gap-8">
+        {category.products.map((product) => (
+          <ProductItem
+            key={product.id}
+            product={computeProductTotalPrice(product)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryProducts;
